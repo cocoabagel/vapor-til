6d06b33da3770e6ca6b1063d44313f5454ca5e16
Kazutoshi Baba <kazutoshi.baba@gmail.com>
2018-06-27 22:28:24 +0900

    Add sibling relationships

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,4 +13,9 @@ public func routes(_ router: Router) throws {
     
     let usersController = UsersController()
     try router.register(collection: usersController)
+    
+    let categoriesController = CategoriesController()
+    try router.register(collection: categoriesController)
+    
+    
 }
